fe0cf280f35db022a45c9f4e9f0f9c454c211ebd
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2020-03-13 10:56:17 +0900

    Error Message

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">ID : </label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">PASSWORD : </label>
+      <input id="password" type="text" v-model="password" />
+    </div>
+    <button>LOGIN</button>
+    <p>{{ logMessage }}</p>
+  </form>
+</template>
+
+<script>
+import { loginUser } from '@/api';
+export default {
+  data() {
+    return {
+      // form values
+      username: '',
+      password: '',
+      // log
+      logMessage: '',
+    };
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const { data } = await loginUser({
+          username: this.username,
+          password: this.password,
+        });
+        this.logMessage = `${data.user.username}님 환영합니다.`;
+      } catch (error) {
+        this.logMessage = error.response.data;
+      } finally {
+        this.initForm();
+      }
+    },
+    initForm() {
+      this.username = '';
+      this.password = '';
+    },
+  },
+};
+</script>
+
+<style></style>
