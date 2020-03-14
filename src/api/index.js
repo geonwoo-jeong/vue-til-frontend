b1431471b2295412f6076b92b3738810e27de852
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2020-03-14 21:15:57 +0900

    post detail

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,38 @@
 import axios from 'axios';
 import { setInterceptors } from './common/interceptors';
 
+/**
+ * Axios Instance Initialize
+ */
 function createInstance() {
   const instance = axios.create({
     baseURL: process.env.VUE_APP_API_URL,
   });
   return setInterceptors(instance);
 }
-
 const instance = createInstance();
 
+/**
+ * Member Register API
+ * @param {userData} userData
+ */
 function registerUser(userData) {
   return instance.post('signup', userData);
 }
 
+/**
+ * Member Login API
+ * @param {userData} userData
+ */
 function loginUser(userData) {
   return instance.post('login', userData);
 }
 
-export { registerUser, loginUser };
+/**
+ * Get Learn Note API
+ */
+function fetchPosts() {
+  return instance.get('posts');
+}
+
+export { registerUser, loginUser, fetchPosts };
