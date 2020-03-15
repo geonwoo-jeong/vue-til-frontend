d4ece18b5b4cf2ada489c8aa515047c184183164
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2020-03-15 14:57:07 +0900

    Loading Spinner

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="main list-container contents">
     <h1 class="page-header">Today I Learned</h1>
-    <ul>
+    <loading-spinner v-if="isLoading"></loading-spinner>
+    <ul v-else>
       <post-list-item
         v-for="postItem in postItems"
         :key="postItem._id"
@@ -13,20 +14,25 @@
 
 <script>
 import PostListItem from '@/components/posts/PostListItem.vue';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
 import { fetchPosts } from '@/api';
 
 export default {
   components: {
     PostListItem,
+    LoadingSpinner,
   },
   data() {
     return {
       postItems: [],
+      isLoading: false,
     };
   },
   methods: {
     async fetchNotes() {
+      this.isLoading = true;
       const { data } = await fetchPosts();
+      this.isLoading = false;
       this.postItems = data.posts;
     },
   },
