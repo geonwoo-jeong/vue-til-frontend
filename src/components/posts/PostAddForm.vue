b0f763c469fe0354b4470dd5a44699bbb00385f4
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2020-03-15 16:50:46 +0900

    Post Contents

--- src/components/posts/PostAddForm.vue
+++ src/components/posts/PostAddForm.vue
@@ -2,23 +2,56 @@
   <div class="contents">
     <h1 class="page-header">CREATE POST</h1>
     <div class="form-wrapper">
-      <form class="form">
+      <form class="form" @submit.prevent="submitForm">
         <div>
           <label for="title">TITLE:</label>
-          <input id="title" type="text" />
+          <input id="title" type="text" v-model="title" />
         </div>
         <div>
           <label for="contents">CONTENTS:</label>
-          <textarea id="contents" type="text" rows="3" />
+          <textarea id="contents" type="text" rows="3" v-model="contents" />
+          <p v-if="!isContentsValid" class="validation-text warning">
+            Text is Too Long
+          </p>
         </div>
+        <button type="submit" class="btn">Create</button>
       </form>
-      <button type="submit" class="btn">Create</button>
+      <p class="log">
+        {{ logMessage }}
+      </p>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { createPost } from '@/api';
+
+export default {
+  data() {
+    return {
+      title: '',
+      contents: '',
+      logMessage: '',
+    };
+  },
+  computed: {
+    isContentsValid() {
+      return this.contents.length <= 200;
+    },
+  },
+  methods: {
+    async submitForm() {
+      try {
+        await createPost({
+          title: this.title,
+          contents: this.contents,
+        });
+      } catch (error) {
+        this.logMessage = error.response.data.message;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
