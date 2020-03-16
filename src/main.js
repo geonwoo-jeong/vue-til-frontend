f512644abe9c33b39fda7c7776046356290ba73d
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2020-03-16 16:01:38 +0900

    Global Filter

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from '@/routes';
 import store from '@/store';
+import { formatDate } from '@/utils/filters';
 
 Vue.config.productionTip = false;
+Vue.filter('formatDate', formatDate);
 
 new Vue({
   render: h => h(App),
